d577ed7e46544d8b4112b01748b37e87aed3a2db
revanth <revanthrao64@gmail.com>
2026-02-16 19:35:13 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: '/IEEE_Inspect-to-Win_Website_GRR_2026/',
+//   plugins: [react()],
+
+//   build: {
+//     minify: 'esbuild',
+//     cssMinify: false, // Preserve CSS comments
+//     esbuild: {
+//       legalComments: 'inline', // Preserve JavaScript comments
+//     },
+//   },
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+
 export default defineConfig({
-  base: '/IEEE_Inspect-to-Win_Website_GRR_2026/',
-  plugins: [react()],
-
-  css: {
-    devSourcemap: true, // Generate CSS source maps for DevTools
-  },
-
-  build: {
-    minify: 'esbuild',
-    cssMinify: false, // Preserve CSS comments
-    sourcemap: true, // Generate source maps for debugging
-    esbuild: {
-      legalComments: 'inline', // Preserve JavaScript comments
-    },
-  },
-})
+  plugins: [
+    react(),
+    cssInjectedByJsPlugin()
+  ]
+});
