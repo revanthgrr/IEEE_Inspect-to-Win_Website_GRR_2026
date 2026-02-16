fa29db0ce666890920951d45b96799de6bd0706e
revanth <revanthrao64@gmail.com>
2026-02-16 20:06:00 +0530

    src folder

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,30 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   base: '/IEEE_Inspect-to-Win_Website_GRR_2026/',
-//   plugins: [react()],
-
-//   build: {
-//     minify: 'esbuild',
-//     cssMinify: false, // Preserve CSS comments
-//     esbuild: {
-//       legalComments: 'inline', // Preserve JavaScript comments
-//     },
-//   },
-// })
-
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    cssInjectedByJsPlugin()
-  ]
-});
+  base: '/IEEE_Inspect-to-Win_Website_GRR_2026/',
+  plugins: [react()],
+
+  build: {
+    minify: 'esbuild',
+    cssMinify: false, // Preserve CSS comments
+    sourcemap: true, // Make source files visible in DevTools
+    esbuild: {
+      legalComments: 'inline', // Preserve JavaScript comments
+    },
+  },
+})
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     cssInjectedByJsPlugin()
+//   ]
+// });
